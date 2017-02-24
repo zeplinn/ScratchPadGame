0f24812ae5733712d5e6c98d3ecdfb34aae03986
Jesper Rytter Jensen <jrytterj@hotmail.com>
2017-02-24 17:59:29 +0100

    draw simple triangle

--- ScratchpadGame/ScratchpadGame.cpp
+++ ScratchpadGame/ScratchpadGame.cpp
@@ -1,11 +1,35 @@
+#include <gl\glew.h>
+#include <cassert>
 #include <QtWidgets\qapplication.h>
-#include <QtWidgets\qwidget.h>
+#include <QtWidgets\qopenglwidget.h>
 #include "ScratchpadGame.h"
+void ScratchpadGame::initializeGL()
+{
+	if(glewInit()==0){
+		assert("glew faild to initialize");
+	}
 
+	glGenBuffers(1, &vertexBufferID);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+	float verts[] = {
+		+0.0f, +0.1f,
+		-0.1f, -0.1f,
+		+0.1f, -0.1f
+	};
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+
+}
+void ScratchpadGame::paintGL()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, nullptr);;
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+}
 int main(int argc,char* argv[])
 {
 	QApplication app(argc, argv);
-	QWidget widget;
+	ScratchpadGame widget;
 	widget.show();
 	return app.exec();
 }
--- ScratchpadGame/ScratchpadGame.h
+++ ScratchpadGame/ScratchpadGame.h
@@ -1,11 +1,13 @@
 #ifndef SCRATCHPAD_GAME_SCRATCHPAD_GAME_H
 #define SCRATCHPAD_GAME_SCRATCHPAD_GAME_H
 
-//class ScratchpadGame : QApplication
-//{
-//protected:
-//	
-//};
+class ScratchpadGame : public QOpenGLWidget
+{
+	GLuint vertexBufferID=0;
+protected:
+	void initializeGL() override;
+	void paintGL() override;
+};
 
 #endif // !SCRATCHPAD_GAME_SCRATCHPAD_GAME_H
 #define SCRATCHPAD_GAME_SCRATCHPAD_GAME_H
